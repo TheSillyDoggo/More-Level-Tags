c12b439e6a42f77d05f2f86513c7a42384d352b3
Explodingbill <talgood09@gmail.com>
2024-01-29 19:04:48 +1100

    fix invisible cursor

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@ using namespace geode::prelude;
 CCMenu* menu;
 LoadingCircle* loading;
 CCNode* l;
+std::vector<CCNode*> buttons;
 
 class $modify(LevelInfoLayer)
 {
 	bool init(GJGameLevel* p0, bool p1)
 	{
+		buttons.clear();
+
 		if (!LevelInfoLayer::init(p0, p1))
 			return false;
 
@@ -31,22 +34,54 @@ class $modify(LevelInfoLayer)
 		ship->setScale(3.5f);
 
 		l = CircleButtonSprite::create(ship, CircleBaseColor::Cyan, CircleBaseSize::Large); // CCSprite::createWithSpriteFrameName("GJ_dislikesIcon_001.png");
+		l->setID("legacy-ship");
+		buttons.push_back(l);
 		menu->addChild(l);
+
+		if (p0->m_twoPlayerMode)
+		{
+			auto cube = CCSprite::createWithSpriteFrameName("portal_03_extra_2_001.png");
+			//auto cube = CCSprite::createWithSpriteFrameName("GJ_dislikesIcon_001.png");
+			cube->setScale(3.5f);
+
+			auto p2 = CircleButtonSprite::create(cube, CircleBaseColor::Pink, CircleBaseSize::Large);
+			p2->setID("2-player");
+
+			menu->addChild(p2);
+			
+			buttons.push_back(p2);
+		}
 
 		this->addChild(menu);
 
 		if (p0->m_levelString.size() > 1)
 		{
 			auto pl = PlayLayer::create(p0, true, true);
+			CCDirector::get()->getOpenGLView()->showCursor(true);
+			delete pl;
 
 			l->setVisible(!pl->m_levelSettings->m_propertykA32);
-			loading->setVisible(false);
+
+			loading->removeFromParent();
+			if (!l->isVisible())
+			{
+				l->removeFromParent();
+				buttons.erase(std::remove(buttons.begin(), buttons.end(), l), buttons.end());
+			}
 		}
 		else
 		{
+			buttons.push_back(l);
+			
 			l->setVisible(false);
 			loading->setVisible(true);
 		}
+
+		for (size_t i = 0; i < buttons.size(); i++)
+		{
+			buttons[i]->setPositionX((18 / 0.15f) * i);
+		}
+		
 
 		return true;
 	}
@@ -62,15 +97,29 @@ class $modify(LevelInfoLayer)
 			if (p0->m_levelString.size() > 1)
 			{
 				auto pl = PlayLayer::create(p0, true, true);
+				CCDirector::get()->getOpenGLView()->showCursor(true);
+				delete pl;
 
 				l->setVisible(!pl->m_levelSettings->m_propertykA32);
 				loading->setVisible(false);
+
+				if (!l->isVisible())
+				{
+					l->removeFromParent();
+					buttons.erase(std::remove(buttons.begin(), buttons.end(), l), buttons.end());
+				}
 			}
 			else
 			{
 				l->setVisible(false);
 				loading->setVisible(true);
 			}
+
+			for (size_t i = 0; i < buttons.size(); i++)
+			{
+				buttons[i]->setPositionX((18 / 0.15f) * i);
+			}
+
 		}
 	}
 };
